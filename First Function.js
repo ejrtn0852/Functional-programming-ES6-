d3da727ad98fcfac206e71710ff72fde30b291ea
ejrtn0852 <ejrtn0852@naver.com>
2023-10-07 10:14:20 +0900

    chapter 3

---
+++ First Function.js
@@ -0,0 +1,15 @@
+// # 일급 함수
+//     -함수를 값으로 다룰 수 있다.
+//     -조합성과 추상화의 도구
+const log = console.log;
+const a = a => a + 5 ;
+log(add5);
+log(add5(5));
+
+const f1 = () => () => 1;
+log(f1());
+
+const f2 = f1();
+log(f2);
+log(f2());
+

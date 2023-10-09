219aef8fb40624aab02c2c2236ed8a00d3a2ed51
ejrtn0852 <ejrtn0852@naver.com>
2023-10-09 15:16:17 +0900

    folder

--- ES6에서의 순회와 이터러블:이터레이터 프로토콜/ Evaluation and First Class.js
+++ ES6에서의 순회와 이터러블:이터레이터 프로토콜/ Evaluation and First Class.js
@@ -1,19 +1,16 @@
-
 // - 코가 계산(Evaluation) 되어 값을 만드는 것
 
-// # 일급 
+// # 일급
 //     - 값으로 다룰 수 있다.
 //     - 변수에 담을 수 있다.
 //     - 함수의 인자로 사용될 수 있다.
 //     - 함수의 결과로 사용될 수 있다.
 
 const a = 10;
-const add10 = a => a +10;
-add10(a); 
-//todo 값으로 다루는 예 
-const log  = console.log;
+const add10 = (a) => a + 10;
+add10(a);
+//todo 값으로 다루는 예
+const log = console.log;
 const r = add10(a);
 log(r);
-//todo 값으로 다루는 예 
-
-
+//todo 값으로 다루는 예
---
+++ 제너레이터와 이터레이터/Generator and Iterator.js
@@ -0,0 +1,17 @@
+const log = console.log;
+
+// 제네레이터/이터레이터
+// - 제네레이터 : 이터레이터이자 이터러블을 생성하는 함수
+
+function* gen() {
+    yield 1;
+    if (!false) yield 2;
+    yield 3;
+    return 100;
+}
+
+let iter = gen();
+log(iter[Symbol.iterator]() == iter);
+
+// 이 제네레이터라는 문장을 통해 순회할 수 있는 값을 만들 수 있다는 이야기는 어떠한 값도 순회할 수 있는 형태로 이 Generator라는 문장을 통해 조작할 수 있으며
+// Generator를 통해서 굉장히 다양한 값들을 순회할 수 있는 iterable을 쉽게 프로그래머가 로직을 만들어가면서 순회를 시킬수 있다는 이야기다.
